cfa29a672a28fbb61d5132010294960c33e66b20
TRALP-CNPq <mkrieger_cnpq@lambda.ele.puc-rio.br>
2014-07-03 15:17:32 -0300

    iOS Project Files added.

---
+++ TRALP_PrototypeTests/TRALP_PrototypeTests.m
@@ -0,0 +1,34 @@
+//
+//  TRALP_PrototypeTests.m
+//  TRALP_PrototypeTests
+//
+//  Created by Tralp Cnpq on 6/16/14.
+//  Copyright (c) 2014 TRALP. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TRALP_PrototypeTests : XCTestCase
+
+@end
+
+@implementation TRALP_PrototypeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
